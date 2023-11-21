4201fd2754ad7626bdd3260de0ceac244275c3f7
Maxim Nabatov <>
2023-11-21 19:46:54 +0300

    init

---
+++ src/main/java/com/example/testassignmentapp/TestAssignmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.testassignmentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAssignmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAssignmentAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testassignmentapp/TestAssignmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testassignmentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAssignmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
